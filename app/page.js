30885c93bec7e4f84868f986ec5d3e9a00602b2d
Lwonbin <lwb9036@gmail.com>
2025-01-23 17:58:01 +0900

    set: main 통합용

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import {MessageForm} from "@/components/share-comps/MessageForm";
 export default function Home() {
     return (
         <>
-            <SearchFrom defaultValue={"검색하기"}/>
+            <SearchFrom defaultValue={"검색하기"}/> 
             <MessageForm defaultValue={"gag"}/>
         </>
     );
