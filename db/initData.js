3f2ad51a5b02e5d64732ef5e3f0d185434d9ac8a
sungjoon <dkdldrha@naver.com>
2025-01-24 15:23:08 +0900

    feat: 예시 데이터 저장

---
+++ db/initData.js
@@ -0,0 +1,19 @@
+export const sideBarData = [{
+    imageUrl: "/friend.png",
+    name: "1",
+
+
+},
+    {
+        imageUrl: "/assets/status-online.svg",
+        name: "2",
+    },
+    {
+        imageUrl: "/assets/status-offline.svg",
+        name: "3",
+    },
+    {
+        imageUrl: "/assets/discord_grey.png",
+        name: "4",
+    },
+];
