4a1910cf724052127d88958c542a64bccecbb869
sungjoon <dkdldrha@naver.com>
2025-02-13 11:22:13 +0900

    전체 구조 ui 작업 #19
    css 설정

--- components/channel-list-comps/SidebarSection.jsx
+++ components/channel-list-comps/SidebarSection.jsx
@@ -5,10 +5,10 @@ const SidebarItem = ({ icon, label, isActive, href }) => {
   return (
 
       <div
-        className={`flex items-center p-2 rounded-md cursor-pointer min-w-[160px] ${
+        className={`flex items-center p-2 mt-1 rounded-md cursor-pointer min-w-[160px] ${
           isActive
             ? 'bg-gray-600 text-white'
-            : 'bg-discordGray text-gray-300 hover:bg-gray-600 hover:text-white'
+            : 'bg-discord2and3 text-gray-300 hover:bg-gray-600 hover:text-white'
         }`}
       >
         <Image src={icon} alt={label} width={24} height={24} />
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,12 @@ export default {
         discordGray: "#2f3136",
         discordSidebar: "#202225",
         discordAccent: "#5865f2",
-        discordGreen:"#22a559"
+        discordGreen:"#22a559",
+        discord2and3: "#2b2d31",
+        discord1and4: "#313338"
+      },
+      boxShadow: {
+        elevationLow: '0 1px 0 hsl(0 calc(1 * 0%) 0.784% / 0.2), 0 1.5px 0 hsl(240 calc(1 * 7.692%) 2.549% / 0.05), 0 2px 0 hsl(0 calc(1 * 0%) 0.784% / 0.05)',
       },
   },
   plugins: [],
