aa55de67735d9aef1850517e5533be517531cc7f
sungjoon <dkdldrha@naver.com>
2025-02-13 17:32:44 +0900

    width tailwind추가

--- ui/sectionTwoAndThree.jsx
+++ ui/sectionTwoAndThree.jsx
@@ -1,6 +1,6 @@
 export function SectionTwoAndThree({children}) {
     return (
-        <div className="flex flex-col h-full w-[240px] bg-discord2and3 relative ">
+        <div className="flex flex-col h-full w-60 bg-discord2and3 relative ">
             {children}
         </div>
     );
